5c696aa5aede1c9a55184cc39d74ee85c518122a
Romas <ruslanas@LT-NB-076.webmedia.int>
2017-04-18 10:32:36 +0300

    Race controller

--- java-autonomous-liferaft/src/main/java/com/nortal/race/assignment/controller/RaceController.java
+++ java-autonomous-liferaft/src/main/java/com/nortal/race/assignment/controller/RaceController.java
@@ -1,6 +1,7 @@
 package com.nortal.race.assignment.controller;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 import com.nortal.race.assignment.model.*;
@@ -12,19 +13,44 @@ public class RaceController {
     }
 
     private List<Point> targetsToCapture = null;
+    private List<Point> takenPoints = null;
+    private double distance;
+    private Point point;
 
     public VesselCommand calculateNextCommand(Vessel vessel, RaceArea raceArea) {
         VesselCommand vesselCommand = new VesselCommand(Thruster.FRONT, ThrusterLevel.T2);
-        vessel.setSpeedX(1.6);
-        vessel.setSpeedY(1.6);
-        for (Point targetPoint : raceArea.getTargets()) {
-            if (targetPoint.equals(vessel.getPosition())) {
-                targetsToCapture.add(targetPoint);
-            } else {
-            }
+        RaceResult raceResult = new RaceResult();
+        if (raceResult.getStatus() == null) {
+            raceResult.startRace();
         }
 
-        // TODO: Implement algorithm to return command for the vessel to capture all the targets on the raceArea provided
+        double maxValue = 1000000000;
+
+        while (!raceResult.isRaceOver()) {
+            for (Point targetPoint : raceArea.getTargets()) {
+                if (takenPoints.contains(targetPoint)) {
+                    continue;
+                } else {
+                    Point position = vessel.getPosition();
+                    distance = Line2D.ptLineDist(position.getX(),
+                            position.getY(),
+                            position.getX(),
+                            position.getY(),
+                            targetPoint.getX(),
+                            targetPoint.getY());
+                }
+                if (distance < maxValue) {
+                    maxValue = distance;
+                    point = targetPoint;
+                }
+            }
+        }
+        if (point == null) {
+            raceResult.setStatus(RaceResult.Status.COMPLETED);
+            return vesselCommand;
+        }
+
+
         return vesselCommand;
     }
 
